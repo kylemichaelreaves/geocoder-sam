7652ec68c5c5381894f379e20bebd1ae5c1557d2
Kyle <kylereaves@gmail.com>
2023-01-26 14:28:39 -0500

    Redoing the function…

--- handler/app.rb
+++ handler/app.rb
@@ -1,4 +1,3 @@
-# require 'httparty'
 require 'json'
 require 'logger'
 require 'geocoder'
@@ -12,39 +11,31 @@ def lambda_handler(event:, context:)
   logger.info(event)
   event.to_a
 
-  # if the queryStringParameters are not present, return an error
-  if event['queryStringParameters'].nil? || event['queryStringParameters'].empty?
-    { statusCode: 400, body: JSON.generate({ message: 'Missing queryStringParameters' }) }
-  end
+  # check for the address object on the queryStringParameters
+  # if the address object is present, use it to build the address
+  if event['queryStringParameters'] && event['queryStringParameters']['address']
+    #   build the address from the address object by iterating over it
+    joined_address = event['queryStringParameters']['address'].map do |key, value|
+      key == 'aptNum' ? "#{value} " : "#{value}, "
+    end.compact
 
-  street_address = event['queryStringParameters']['street_address']
-  apt_num = event['queryStringParameters'].fetch('apt_num', '')
-  city = event['queryStringParameters']['city']
-  state = event['queryStringParameters']['state']
-  zipcode = event['queryStringParameters'].fetch('zipcode', '')
-
-  address = "#{street_address} #{apt_num}, #{city}, #{state} #{zipcode}"
-
-  # if all the elements in the array are empty or nil, return an error
-  if address.all?(&:empty?)
-    { statusCode: 400, body: JSON.generate({ message: 'An address could not be formed from the queryStringParameters' }) }
-    break
-  else
-
-    results = Geocoder.search(address)
-
-    results.headers = {
-      'Access-Control-Allow-Origin' => '*',
-      'Access-Control-Allow-Headers' => 'Content-Type',
-      'Access-Control-Allow-Methods' => 'OPTIONS,POST,GET'
-    }
-
+    results = Geocoder.search(joined_address)
+    # if no results, return an error
+    # otherwise, return the results
     if results.empty?
-      { statusCode: 200, body: JSON.generate({ message: "No results found for #{address}" }) }
+      { statusCode: 400,
+        headers: {
+          'Content-Type' => 'application/json'
+        },
+        body: JSON.generate({ message: "No results found for #{address}" }) }
     else
-      { statusCode: 200, body: JSON.generate({ message: results.to_json }) }
+      { statusCode: 200,
+        headers: {
+          'Content-Type' => 'application/json'
+        },
+        body: JSON.generate({ message: results.to_json }) }
     end
 
   end
-
 end
+
