32ab1a3cdfcfd0336acaf7fa1e47f036041034d9
Kyle <kylereaves@gmail.com>
2023-03-19 18:39:53 -0400

    Refactoring handler

--- handler/app.rb
+++ handler/app.rb
@@ -9,55 +9,69 @@ def lambda_handler(event:, context:)
   logger.info(ENV.to_a)
   logger.info('## EVENT')
   logger.info(event)
+  logger.info('## CONTEXT')
+  logger.info(context)
   # check for the address object on the queryStringParameters
   # if the address object is present, use it to build the address
-  if event['queryStringParameters'] && event['queryStringParameters']['address']
-    query_string = event['queryStringParameters']['address']
-    if query_string.empty?
-      logger.info('The query_string was empty.')
-      return { statusCode: 400,
-               headers: { 'Content-Type' => 'application/json' },
-               body: JSON.generate({ message: 'The query_string was empty.' })
-      }
+  if event['queryStringParameters'] && !event['queryStringParameters'].empty?
+    address_hash = {}
+    # event['queryStringParameters'].each do |key, value|
+    #   address_hash[key.downcase] = value
+    # end
+    address_keys = %w[streetAddress unitOrAptNum municipality state zipcode]
+
+    # iterate over the address_keys list and construct the address_hash accordingly
+    address_keys.each do |key|
+      if event['queryStringParameters'][key]
+        address_hash[key.downcase] = event['queryStringParameters'][key]
+      end
     end
-    logger.info("query_string: #{query_string}")
-    address_hash = JSON.parse(query_string)
-    logger.info(`address_hash: #{address_hash}`)
-    # concat a string from the address_hash by iterating over it
+    logger.info("address_hash: #{address_hash}")
+
+    # build the joined address
     joined_address = address_hash.map do |key, value|
-      key == 'aptNum' ? "#{value} " : "#{value},"
-    end.compact
-    # if an address cannot be built from the address for whatever reason, return an error
-    if joined_address.all?(&:empty?)
-      { statusCode: 400,
-        headers: {
-          'Content-Type' => 'application/json'
-        },
-        body: JSON.generate({ message: 'the joined_address was empty' })
+      key == 'aptorunitnum' ? "#{value} " : "#{value},"
+    end.compact.join("")
+    logger.info("joined_address: #{joined_address}")
+
+    # if the joined address is empty, return an error
+    if joined_address.empty?
+      error_message = 'The address object was empty'
+      logger.error(error_message)
+      return {
+        statusCode: 400,
+        headers: { 'Content-Type' => 'application/json' },
+        body: JSON.generate({ message: error_message })
       }
     end
-    # join the address array into a string
-    joined_address = joined_address.join("")
-    results = Geocoder.search(joined_address)
-    # if no results, return an error
-    # otherwise, return the results
-    if results.empty?
-      { statusCode: 400,
-        headers: {
-          'Content-Type' => 'application/json'
-        },
-        body: JSON.generate({ message: "No results found for #{joined_address}" }) }
-    else
-      # log the results
-      logger.info("results: #{results.to_json}")
-      # iterate over the results, returning each result as a JSON object
-      results.each { |result| logger.info(result.data.to_json) }
-      { statusCode: 200,
-        headers: {
-          'Content-Type' => 'application/json'
-        },
-        body: JSON.generate({ message: results.each { |res| res.data.to_json } }) }
+
+    # search for the address using the Geocoder gem
+    begin
+      results = Geocoder.search(joined_address)
+      if results.empty?
+        logger.warn("No results found for #{joined_address}")
+        return {
+          statusCode: 404,
+          headers: { 'Content-Type' => 'application/json' },
+          body: JSON.generate({ message: "No results found for #{joined_address}" })
+        }
+      else
+        logger.info("Found #{results.count} results for #{joined_address}")
+        { statusCode: 200,
+          headers: {
+            'Content-Type' => 'application/json'
+          },
+          body: JSON.generate({ message: results.map { |res| res.data } })
+        }
+      end
+    rescue => e
+      error_message = "Geocoder search failed: #{e.message}"
+      logger.error(error_message)
+      {
+        statusCode: 500,
+        headers: { 'Content-Type' => 'application/json' },
+        body: JSON.generate({ message: error_message })
+      }
     end
   end
 end
-
